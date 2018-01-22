24dc4a150585480c158f5d62aebd3d93e2c344f0
Dan Caddigan <goldcaddy77@gmail.com>
2018-01-22 02:54:38 +0000

    Updates

---
+++ dist/types/components/ReblocksPayment/BeerForm.d.ts
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { PaymentResponse } from './ReblocksPayment';
+export interface Props {
+    onSuccess: (data: PaymentResponse) => void;
+}
+export interface State {
+    currentXRBPrice?: number;
+    xrbPerBeer?: number;
+}
+export declare class BeerForm extends React.Component<Props, State> {
+    BEER_PRICE: number;
+    constructor(props: Props);
+    componentDidMount(): void;
+    loadData: (currency?: "AUD" | "BRL" | "CAD" | "CHF" | "CLP" | "CNY" | "CZK" | "DKK" | "EUR" | "GBP" | "HKD" | "HUF" | "IDR" | "ILS" | "INR" | "JPY" | "KRW" | "MXN" | "MYR" | "NOK" | "NZD" | "PHP" | "PKR" | "PLN" | "RUB" | "SEK" | "SGD" | "THB" | "TRY" | "TWD" | "USD" | "ZAR" | undefined) => void;
+    formatUSD: (value?: number | undefined) => string;
+    render(): JSX.Element;
+}
